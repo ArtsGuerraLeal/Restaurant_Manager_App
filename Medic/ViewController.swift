088a251ba6ffe21c7ff03bf934ace7136b0e06d7
artguerra <guerraarturo11@gmail.com>
2020-06-25 00:25:47 -0500

    First Commit

---
+++ Medic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Medic
+//
+//  Created by Arturo Guerra on 12/15/19.
+//  Copyright © 2019 Arturo Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
